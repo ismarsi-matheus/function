c62b448d28feaae06d565c59d78c92366ae9521b
ismarsi-matheus <matheusleo.senac@gmail.com>
2024-08-21 00:34:42 -0300

    Update exercsio1.js

--- exercisios function/exercsio1.js
+++ exercisios function/exercsio1.js
@@ -1,7 +1,9 @@
-//faça a tabuada do 5
+ function tabuada(contador){
 contador = 1
 
 while (contador <= 10){
-    console.log("5X", contador, "=", contador * 7)
+    console.log("5X", contador, "=", contador * 5)
     contador++
 }
+  }
+tabuada()
